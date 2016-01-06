8f9bbe63c717f3f8a51d6d7f20dc80fbb5e61f1c
Joe Minock <joe.minock@gmail.com>
2016-01-06 10:53:20 -0700

    updated project model and index views

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,7 +8,7 @@ class Project < ActiveRecord::Base
 
 	has_many :tasks
 
-	validates :name, presence: true, length: {maximum: 75}
+	validates :name, presence: true, length: {maximum: 40}
 	validates :s_description, presence: true, length: {maximum: 150}
 	validates :l_description, presence: true, length: {maximum: 500}
 	validates :price, presence: true, numericality: { only_integer: true }
