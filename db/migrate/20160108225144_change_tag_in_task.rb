d13c49e236947714e4c9388d639c35fd43b6ab52
Joe Minock <joe.minock@gmail.com>
2016-01-08 16:13:43 -0700

    fixed migration update, should work this time

--- db/migrate/20160108225144_change_tag_in_task.rb
+++ db/migrate/20160108225144_change_tag_in_task.rb
@@ -1,5 +1,5 @@
 class ChangeTagInTask < ActiveRecord::Migration
   def change
-  	change_column :tasks, :tag, 'integer USING CAST(column_name AS integer)'
+  	change_column :tasks, :tag, 'integer USING CAST(tag AS integer)'
   end
 end
