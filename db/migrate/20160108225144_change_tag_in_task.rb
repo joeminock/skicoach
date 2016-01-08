87f1d3a212efadc9311896d58cf0c09eee4bf9f7
Joe Minock <joe.minock@gmail.com>
2016-01-08 16:10:45 -0700

    fixed migration, change data type string to integer

--- db/migrate/20160108225144_change_tag_in_task.rb
+++ db/migrate/20160108225144_change_tag_in_task.rb
@@ -1,5 +1,5 @@
 class ChangeTagInTask < ActiveRecord::Migration
   def change
-  	change_column :tasks, :tag, :integer, 'integer USING CAST(column_name AS integer)'
+  	change_column :tasks, :tag, 'integer USING CAST(column_name AS integer)'
   end
 end
