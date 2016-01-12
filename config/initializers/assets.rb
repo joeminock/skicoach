ce0ee1d3696b2c8364fcd280575032e1f246ed4c
Joe Minock <joe.minock@gmail.com>
2016-01-11 23:11:08 -0700

    fixed asset

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( avatar.jpg )
-Rails.application.config.assets.precompile += %w( star-half.jpg )
-Rails.application.config.assets.precompile += %w( star-off.jpg )
-Rails.application.config.assets.precompile += %w( star-on.jpg )
+Rails.application.config.assets.precompile += %w( star-half.png )
+Rails.application.config.assets.precompile += %w( star-off.png )
+Rails.application.config.assets.precompile += %w( star-on.png )
