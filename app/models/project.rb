97563b9382c2e3e03fab1d7270e7276cfb6c8dae
Joe Minock <joe.minock@gmail.com>
2016-01-11 23:01:13 -0700

    cleaned up views

--- app/models/project.rb
+++ app/models/project.rb
@@ -11,7 +11,7 @@ class Project < ActiveRecord::Base
 
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :s_description, presence: true, length: { maximum: 140 }
-	validates :l_description, presence: true, length: { maximum: 500 }
+	validates :l_description, presence: true, length: { maximum: 1000 }
 	validates :price, presence: true, numericality: { only_integer: true }
 
 	has_attached_file :image, :styles => { :medium => "680x300>", :thumb => "170x75>" }
