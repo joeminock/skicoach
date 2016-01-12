364fe674ad31010ed04e11abb3a91ca4fed080a9
Joe Minock <joe.minock@gmail.com>
2016-01-12 12:08:04 -0700

    reset migrations to last push to heroku

--- db/migrate/20160112164038_change_task_time_in_tasks.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTaskTimeInTasks < ActiveRecord::Migration
-  def change
-  	change_column :tasks, :task_time, :time 
-  end
-end
--- db/migrate/20160112175600_remove_task_time_from_tasks.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTaskTimeFromTasks < ActiveRecord::Migration
-  def change
-  	change_column :tasks, :task_time, :integer
-  end
-end
