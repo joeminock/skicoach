402402436e272c1dea1b7e07f5985e767df216db
Joe Minock <joe.minock@gmail.com>
2017-10-09 22:08:08 -0600

    Trying to fix mailer error

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,10 @@ class Project < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :name, use: [:slugged, :finders]
 
+	def should_generate_new_friendly_id?
+    name_changed?
+  end
+
 	has_many :tasks
 
 	has_many :subscriptions
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User < ActiveRecord::Base
 
   after_create :send_notification
 
- def send_notification
- 	MyMailer.new_user(self).deliver
- end
+ # def send_notification
+ #  MyMailer.new_user(self).deliver
+ # end
 
   def self.find_for_google_oauth2(access_token, signed_in_resourse=nil)
   	data = access_token.info
