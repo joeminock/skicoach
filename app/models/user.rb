32dbefe62deb7bec7bcda9ba1d1ccbc30aac80dd
Joe Minock <joe.minock@gmail.com>
2017-10-09 22:11:10 -0600

    Remove all mailers

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 
   has_many :reviews
 
-  after_create :send_notification
+  #after_create :send_notification
 
  # def send_notification
  #  MyMailer.new_user(self).deliver
